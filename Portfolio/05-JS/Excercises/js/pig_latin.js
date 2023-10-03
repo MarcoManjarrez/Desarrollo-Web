32a2b4b8676587a24468dc9f111fc4fec93689a0
DemianVGLl2 <0253139@up.edu.mx>
2023-10-03 09:03:55 -0600

    Update: 02/10/2023

--- Portfolio/05-JS/Excercises/js/pig_latin.js
+++ Portfolio/05-JS/Excercises/js/pig_latin.js
@@ -1,19 +1,36 @@
 function piglatin() {
     "use strict";
-    var n = document.getElementById("num").value;
-    var val = f(n);
-    document.getElementById("pigLbl").textContent = val;
-}
+    const text = document.getElementById("text").value;
+    var val = "";
 
-function f(n) {
-    var value;
-    // Check if the memory array already contains the requested number
-    if (memo.hasOwnProperty(n)) {
-        value = memo[n];
-    } else {
-        value = f(n - 1) + f(n - 2);
-        memo[n] = value;
-    }
+     // Define a regular expression pattern to match words (letters separated by non-letters)
+     const wordPattern = /(\w+)(\W*)/g;
 
-    return value;
+     // Split the input text into words using the regular expression
+     const words = text.match(wordPattern);
+ 
+     if (!words) {
+         val= ""; // No words found
+     }
+     else {
+        // Define a function to translate a single word to Pig Latin
+        function translateWord(word) {
+            const firstLetter = word[0];
+            const restOfWord = word.slice(1);
+            const vowels = "aeiouAEIOU";
+    
+            if (vowels.includes(firstLetter)) {
+                return word + "ay"; // Word starts with a vowel
+            } else {
+                return restOfWord + firstLetter + "ay"; // Word starts with a consonant
+            }
+        }
+    
+        // Translate each word and join them back into a sentence
+        const translatedWords = words.map(translateWord);
+        val += translatedWords;
+     }
+
+     document.getElementById("pigLbl").textContent = val;
+     
 }
