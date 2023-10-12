b90fedf2002fc48d355edf9604c71a8b94297fca
DemianVGLl2 <0253139@up.edu.mx>
2023-10-12 10:46:52 -0600

    Update: 12/10/2023

--- Portfolio/08-Express/server.js
+++ Portfolio/08-Express/server.js
@@ -10,14 +10,26 @@ app.engine('ejs', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 const family = ["Demian", "Julieta", "Jimena", "Ernestina", "Cristian"];
+let name = "Demian";
 
 app.get('/', (req, res) => {
-    res.render("index", { name: "Demian", family: family});
+    res.render("index", { name: name });
 });
 
-app.get('/about', (req, res) => {
-    var name = req.query.name;
-    res.send("<h2>Hello "+ name +" from query in url</h2>");
+app.get('/about', (req, res, next) => {
+    var loc_name = req.query.name;
+    const error = new Error("Missing 'name' value");
+    error.status = 400;
+    if (!loc_name) next(error);
+    else {
+        name = loc_name;
+        res.redirect("/");
+    }
+}, (error, req, res, next) => {
+    console.error(error.stack);
+    res.status(500).render("error", {
+       message: error.status + " = " + error.message
+    });
 });
 
 app.get('/about/:name/detail', (req, res) => {
@@ -29,7 +41,22 @@ app.route('/login')
     .get((req, res) => {
         var username = req.query.username;
         var password = req.query.password;
-        res.send("<h2>Hello "+ username +", you are logged in</h2>")
+        res.send("<h2>Hello "+ username +", you are logged in</h2>");
+
+        if (!username) {
+            const error = new Error("Missing 'name' value");
+            error.status = 400;
+            next(error);
+        }
+        else {
+            name = loc_name;
+            res.redirect("/");
+        }
+    }, (error, req, res, next) => {
+        console.error(error.stack);
+        res.status(500).render("error", {
+        message: error.status + " = " + error.message
+        });
     })
 
     .post((req, res) => {
@@ -43,6 +70,22 @@ app.route('/login')
 app.get('/secure', (req, res) => {
     res.send("<h1>This is a secure location<h1>");
 });
+
+/*
+app.use((error, req, res, next) => {
+    console.error(error.stack);
+    res.status(500).send("There was an error in the processing of your request");
+});
+*/
+
+/*
+app.use((error, req, res, next) => {
+    console.error(error.stack);
+    res.status(500).render("error", {
+       message: error.status + " - " + error.message
+    });    
+});
+*/
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
