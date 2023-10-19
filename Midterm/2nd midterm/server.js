8e4eb1b853528b8170dae0b619753dfe9fce2ce7
DemianVGLl2 <0253139@up.edu.mx>
2023-10-19 13:25:39 -0600

    Update: For 2nd midterm - 4

--- Midterm/2nd midterm/server.js
+++ Midterm/2nd midterm/server.js
@@ -14,8 +14,44 @@ app.set('view engine', 'ejs');
 const url1 = "https://anapioficeandfire.com/api/characters";
 const url2 = "https://thronesapi.com/api/v2/Characters";
 
+let id = "";
+let image = "";
+let first_name = "";
+let last_name = "";
+let born_date = "";
+let dead_date = "";
+let titles = [];
+let aliances = "";
+let family = [];
+
 app.get('/', (req, res) => {
-    res.render("index");
+    res.render("index", { id: id });
+});
+
+app.get('/', (req, res) => {
+    https.get(url1, (response) => {
+        console.log(response);
+        let tempRes="";
+
+        response.on('data', (data) => {
+            console.log(data);
+            tempRes+=data;
+        }).on('end', (data) => {
+            var json = JSON.parse(tempRes);
+        });
+    });
+
+    https.get(url2, (response) => {
+        console.log(response);
+        let tempRes="";
+        
+        response.on('data', (data) => {
+            console.log(data);
+            tempRes+=data;
+        }).on('end', (data) => {
+            var json = JSON.parse(tempRes);
+        });
+    });
 });
 
 
