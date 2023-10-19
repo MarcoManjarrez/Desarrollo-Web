506d721b31d0debfbfdee24f26e763907a42850b
DemianVGLl2 <0253139@up.edu.mx>
2023-10-19 10:24:47 -0600

    Update: For 2nd midterm - 3

--- Midterm/2nd midterm/server.js
+++ Midterm/2nd midterm/server.js
@@ -2,3 +2,23 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const app = express();
 
+const https = require("https");
+
+app.use(express.static("public"));
+
+app.use(bodyparser.urlencoded({extended: true}));
+
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+const url1 = "https://anapioficeandfire.com/api/characters";
+const url2 = "https://thronesapi.com/api/v2/Characters";
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+
+app.listen(1000, () => {
+    console.log("Listening to port 1000");
+});
